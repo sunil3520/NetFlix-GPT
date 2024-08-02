e0fe0d5b88b19c76c1a91833426ee1bfe2d91e3d
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2024-08-02 21:50:40 +0530

    fix:MovieCard has been fixed

--- src/components/common/MovieCard.jsx
+++ src/components/common/MovieCard.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 const MovieCard = ({poster_path,title,movieId,index}) => {
   return (
     <Link to={`/browse/${movieId}`}>
-    <div className='cursor-pointer hover:z-50   w-24  md:w-40 rounded-lg hover:scale-150 transition-traslate duration-300 ease-in-out'>
-        <img className=' object-cover'   src={IMAGE_CDN_URL+poster_path} alt={title} />
+    <div className='cursor-pointer hover:z-100  w-24  md:w-40   hover:scale-x-150 hover:scale-y-125 transition-traslate duration-300 ease-in-out'>
+        <img className=' object-cover rounded-lg'   src={IMAGE_CDN_URL+poster_path} alt={title} />
     </div>
     </Link>
   )
--- src/pages/Browse/SecondaryContainer.jsx
+++ src/pages/Browse/SecondaryContainer.jsx
@@ -3,7 +3,7 @@ import MovieList from '../../components/common/MovieList'
 
 const SecondaryContainer = () => {
   return (
-    <div className ="bg-black ">
+    <div className ="bg-black">
      <div className = "mt-0 lg:-mt-[17%] relative z-40">
      <MovieList title ={"Now Playing"}/>
      <MovieList title ={"Popular"}/>
