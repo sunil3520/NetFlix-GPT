1a9babe1f2b23b0ed5a12538af12e908626cb1b3
Sunil Chaudhary <sunilchaudhary7789@gmail.com>
2024-08-10 23:15:12 +0530

    working on responsiveness

--- src/pages/Browse/VideoTitle.jsx
+++ src/pages/Browse/VideoTitle.jsx
@@ -8,10 +8,10 @@ const VideoTitle = ({title,overview,movieId}) => {
     
   return (
     <div className=' absolute pt-[15%] px-12 bg-gradient-to-r from-black text-white w-screen aspect-video'>
-        <h1 className='font-bold text-3xl md:text-6xl'>{title}</h1>
-        <p className=' md:py-6 w-[90%] sm:[80%] md:w-1/3'>{overview}</p>
+        <h1 className='font-bold text-3xl md:text-5xl lg:text-6xl'>{title}</h1>
+        <p className='md:py-6 w-[90%]  lg:w-2/3'>{overview}</p>
         <div className='flex'>
-            <Link to={`/browse/${movieId}`}><button className=' flex items-center gap-4 px-8 cursor-pointer py-2 bg-white text-black rounded-md hover:bg-opacity-80'><span><FaRegPlayCircle className='text-3xl'/></span>Play</button></Link>
+            <Link to={`/browse/${movieId}`}><button className='flex items-center gap-4 px-8 cursor-pointer py-2 bg-white text-black rounded-md hover:bg-opacity-80'><span><FaRegPlayCircle className='text-3xl'/></span>Play</button></Link>
             <button className='ml-2 px-8 py-2 bg-gray-600 cursor-pointer text-black rounded-md hover:bg-opacity-80'>More Info</button>
         </div>
     </div>
